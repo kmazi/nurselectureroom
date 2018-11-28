4338b2ea2c5c2ca85d70061374224e49c6fc3ea0
kmazi <ugochukwu.mazi@andela.com>
2018-11-28 11:57:49 +0100

    feat(auth): Authenticate users via email address 	- Add some other major activities

---
+++ app/src/main/java/com/mazimia/mobile/nurselectureroom/NurseRoomActivity.java
@@ -0,0 +1,13 @@
+package com.mazimia.mobile.nurselectureroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NurseRoomActivity extends SignOutActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nurse_room);
+    }
+}
